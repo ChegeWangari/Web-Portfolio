861d712896e3c44b0893b2d691222316578f3310
ChegeWangari <maweri254@gmail.com>
2025-02-28 18:00:44 +0300

    git config --global user.n

---
+++ MyPortfolio/trial.js
@@ -0,0 +1,15 @@
+/WEB DESIGN AND DEVELOPMENT USING HTML, CSS AND JAVASCRIPT: Page 48 of 68
+<Html>
+<head>
+<script type="text/javascript">
+function sayHello ()
+{
+alert("Hello World");
+}
+</script>
+</head>
+<body>
+Click here for the result
+<input type="button" onClick="sayHello( )" value="Say Hello" />
+</body>
+</Html>
